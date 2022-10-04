5ab855f6bee7568ba53b2b4551f3b6ca30c31009
urbluemn <nyaroshko1404@gmail.com>
2022-10-05 02:45:36 +0300

    Add project files.

---
+++ HomeWork-01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hey, welcome to my code!");
+Console.WriteLine();
+Console.WriteLine("I'm new here so there is almost nothing here...");
+Console.WriteLine();
+Console.WriteLine("Anyways, i can change color for you, here take a look!");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hope you liked it, have a good one!");
---
+++ LibraryHW-01/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace LibraryHW_01
+{
+    public class Class1
+    {
+
+    }
+}
